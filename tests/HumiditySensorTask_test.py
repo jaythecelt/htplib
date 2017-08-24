0215a50cc065ed64b39ea86af9c803f01e55c835
jaythecelt <jaythecelt@gmail.com>
2017-08-24 18:00:24 -0400

    Changed test to continually read sensor

--- tests/HumiditySensorTask_test.py
+++ tests/HumiditySensorTask_test.py
@@ -9,27 +9,48 @@ import time
 import threading
 
 
+humTask = None
+
+def singleShotMode():
+    while True:
+        
+        log.console("\n------------------\nPress Enter to take a reading for ~ 2 sec...")
+        input()
+        ctrQ = counterQueue.CounterDataQueue()
+        ctrQ.clear()
+        
+        humTask = HumiditySensorTask()
+        t = threading.Thread(target=humTask.run, args=())
+        t.start()
+        time.sleep(2)
+        humTask.terminate()
+        
+        while not ctrQ.isEmpty():
+            countTuple = ctrQ.get()
+            log.console("sensor num: {0} \tcount: {1}".format(countTuple[0], countTuple[1] ))
+
+
+def continuousMode():
+
+    try:
+        ctrQ = counterQueue.CounterDataQueue()
+        humTask = HumiditySensorTask()
+        t = threading.Thread(target=humTask.run, args=())
+        t.start()
+
+        while True:        
+            while not ctrQ.isEmpty():
+                countTuple = ctrQ.get()
+                log.console("sensor num: {0} \tcount: {1}".format(countTuple[0], countTuple[1] ))
+    except KeyboardInterrupt:
+        print ("\nTerminating humidity sensor task")
+        humTask.terminate()
+
 
 def main():
 
     try:
-        while True:
-            
-            log.console("\n------------------\nPress Enter to take a reading for ~ 2 sec...")
-            input()
-            ctrQ = counterQueue.CounterDataQueue()
-            ctrQ.clear()
-            
-            humTask = HumiditySensorTask()
-            t = threading.Thread(target=humTask.run, args=())
-            t.start()
-            time.sleep(2)
-            humTask.terminate()
-            
-            while not ctrQ.isEmpty():
-                countTuple = ctrQ.get()
-                log.console("sensor num: {0} \tcount: {1}".format(countTuple[0], countTuple[1] ))
-
+        continuousMode()
     
     except KeyboardInterrupt:
         log.info("\nExiting")
